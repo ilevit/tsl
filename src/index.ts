2b48c5850aaa60b07679c6af75003936a11eae03
ilevit <anon>
2023-09-10 16:52:38 -0400

    Semicolons

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 function onLoad(saveData: any) {
 
     // Lock color-UI cube
-    let cube = getObjectFromGUID("c1a0d1")
-    cube.interactable = false
-    cube.setLock(true)
+    let cube = getObjectFromGUID("c1a0d1");
+    cube.interactable = false;
+    cube.setLock(true);
 }
