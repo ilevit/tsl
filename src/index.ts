ae40428912b9bf6d33c16f6ae4e34f17e767b8db
ilevit <anon>
2023-09-10 14:57:56 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// The OnLoad function. This is called after everything in the game save finishes loading.
+// Most of your script code goes here.
+function onLoad(saveData: any) {
+
+    // Lock color-UI cube
+    let cube = getObjectFromGUID("c1a0d1")
+    cube.interactable = false
+    cube.setLock(true)
+}
